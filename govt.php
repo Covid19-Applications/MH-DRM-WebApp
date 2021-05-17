b697c767a629fa2e3fdacb1b7cbeb59faf028599
Shaharyaar7 <shaharyaarkutchi7@gmail.com>
2021-05-17 22:16:24 +0530

    created modules

---
+++ govt.php
@@ -0,0 +1,140 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<br><br><br><br>
+
+  
+<style type="text/css">
+  
+.button{
+  background: #3498db;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+#services .box {
+    padding: 50px 20px;
+    margin-bottom: 50px;
+    text-align: center;
+    border: 1px solid #e6e6e6;
+    height: 170px;
+    position: relative;
+    background: #fafafa;
+}
+  
+</style>
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                        <img src="img/govt/logo1.png" class="img-fluid" style="height: 150px;">
+                        <h1>Government Services</h1>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                      <br>
+                        <img src="img/govt/govt1.gif" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end  -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title text-center" >Digital Maharashtra </h3>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/govt/1.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://aaplesarkar.mahaonline.gov.in/"> <h4>Citizen Login for RTS Services</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/govt/2.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://aaplesarkar.mahaonline.gov.in/"> <h4>Aaple Sarkar Seva Kendra</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/govt/3.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://maharecruitment.mahaonline.gov.in/"> <h4>Maha-Recruitment Portal</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/govt/4.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://exciseservices.mahaonline.gov.in/"> <h4>Maharashtra State Excise</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/govt/5.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://www.mahaonline.gov.in/Site/44/Department%20Application"> <h4>Government Department</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/govt/6.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://mahampsc.mahaonline.gov.in/"> <h4>MPSC</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/govt/7.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://msrtc.mahaonline.gov.in/"> <h4>MSRTC</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+          
+         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/govt/8.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="https://mmbeticketing.mahaonline.gov.in/"> <h4>Maharashtra Maritime Board</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/govt/9.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="javascript:validate()"> <h4>Verify Authenticated Certificate</h4></a>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
---
+++ responders.php
@@ -0,0 +1,344 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+<style type="text/css">
+    /* line 10, C:/Users/SPN Graphics/Desktop/CL September/210 Medical/HTML/scss/_bradcam.scss */
+.button{
+  background: #E7717D;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+.bg-name{
+  background-color: #FFC7C9;
+}
+.bg-number{
+  background-color: #DA2A47;
+}
+</style>
+    <!-- bradcam_area_start  -->
+    
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br><br><br>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="bradcam_text">
+                      <br><br>
+                        <h1 style="text-decoration: underline;">First Responders</h1>
+                            <a href="#res"><button class="button">Know More</button></a>
+
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="bradcam_text">
+                      <br><br>
+                        <img src="img/responders/bg.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end  -->
+
+	<!-- Start Sample Area -->
+	<section class="sample-text-area">
+		<div class="container box_1170">
+			<h3 class="sample-text" style="text-align: center;">
+                <br><br>
+                Plan, Prepare, and Respond
+			</h3>
+		</div>
+	</section>
+	<!-- End Sample Area -->
+
+
+
+    <div class="whole-wrap">
+        <div class="container box_1170">
+            <div class="jumbotron">
+                <div class="card col-md-12">
+                    
+                    <div class="card-header">
+                        Guidelines
+                     </div>   
+                     <div class="card-body">
+
+                        This guidance applies to all first responders, including law enforcement, fire services, emergency medical services, and emergency management officials, who anticipate close contact with persons with confirmed or possible COVID-19 in the course of their work.
+                      </div>  
+                </div>
+
+                <br><br>
+                <div class="card col-md-12">
+                    
+                    <div class="card-header">
+                        Summary of Key Changes for the EMS Guidance:
+                     </div>   
+                     <div class="card-body">
+
+                        <strong>Updated PPE recommendations for the care of patients with known or suspected COVID-19:</strong>
+
+                        <ul>
+                            <li>Facemasks are an acceptable alternative until the supply chain is restored. Respirators should be prioritized for procedures that are likely to generate respiratory aerosols, which would pose the highest exposure risk to HCP.
+                            Eye protection, gown, and gloves continue to be recommended.</li><br>
+
+                             <li> If there are shortages of gowns, they should be prioritized for aerosol-generating procedures, care activities where splashes and sprays are anticipated, and high-contact patient care activities that provide opportunities for transfer of pathogens to the hands and clothing of HCP.</li><br>
+
+                              <li> When the supply chain is restored, fit-tested EMS clinicians should return to use of respirators for patients with known or suspected COVID-19.</li><br>
+
+                              <li>Updated guidance about recommended EPA-registered disinfectants to include reference to a list now posted on the EPA website.</li>
+                        </ul>
+
+                       
+                    </div>  
+                </div>
+
+                <br><br>
+                 <div class="card col-md-12">
+                    
+                    <div class="card-header">
+                       Responding First as a Leading Power
+                     </div>   
+                     <div class="card-body">
+
+                      The emerging concept of India as a “first responder” reflects the country’s growing capability and
+increasing willingness to assume the role of a leading power. By contributing its resources to prevent
+or mitigate regional and international crises, India is demonstrating its commitment as a responsible
+actor in the international order. Beyond narrow self-interest, such contributions help project India’s
+soft power abroad and portray India in a positive light. They also reflect India’s expanding sphere of
+influence and capacity to shape events abroad.<br><br>
+The international order is facing a variety of transnational challenges that occasionally erupt into
+acute crises. Whether it is a natural catastrophe, an Internet disruption, or a sudden financial shock,
+the repercussions are often massive, with regional and global implications. This is where individual
+states must take immediate action, to mitigate the crisis impact and avoid further escalation. By
+taking the lead, they are providing a public good to the international order, supporting smaller or
+less capable states in dire need of assistance.<br><br>
+As the Indian economy surges on and the country emerges as one of the key actors in the
+international order, expectations are consequently growing about India’s capacity to provide such
+support as a first responder to crises beyond its borders. Commenting on this rising role, Foreign
+Secretary Dr. S. Jaishankar thus emphasizes that India’s foreign policy dimension is “to aspire to be
+a leading power, rather than just a balancing power ... (and) a willingness to shoulder greater global
+responsibilities.”<br><br>
+While the concept of “first responder” has generally been interpreted quite narrowly, focusing on
+humanitarian disasters, a broader definition illustrates how India has played a crucial role in
+assuming these “global responsibilities” by responding to a variety of crises in its neighbourhood
+and beyond. This is particularly apparent in seven issue-areas.<br><br>
+
+                      <div class="card col-md-12">
+                    
+                    <div class="card-header">
+                        Natural disasters
+                     </div>   
+                     <div class="card-body">
+When the forces of nature unleash their fury on South Asia, the Indian government and military
+forces have played a critical role in supporting neighbouring countries in relief operations. After the
+2004 tsunami, India deployed 14 Navy vessels, nearly 1,000 military personnel and several dozen
+helicopters and airplanes to Sri Lanka. In 2007, in the aftermath of cyclone Sidr, India was one of
+the few countries allowed to provide relief to Myanmar and provided critical rice supplies to address
+food emergencies there. In 2015, less than six hours after Nepal was rattled by a tragic earthquake
+taking almost 9,000 lives, the Indian Air Force flew in National Disaster Response Force (NDRF)
+rescue team. Over the next days, India lead on the ground, landing 32 flights with 520 tonnes of
+relief and more than fifty medical, Army engineering and other rescue operation teams. In 2014, the
+Indian Navy was the first to arrive in the Madives to provide fresh drinking water to more than
+150,000 of its citizens facing an acute supply crisis. More recently, in response to Cyclone More
+(2017), India was the first to respond to the devastating floods in Sri Lanka and Bangladesh. As
+reflected in relief provided to Pakistan in 2005 and 2010, Indian support transcends political
+considerations and is driven by a deep humanitarian drive. 
+                       
+                    </div> 
+
+                </div>
+
+                  <div class="card-header">
+                         Expatriate evacuation operations
+                     </div>   
+                     <div class="card-body">
+
+When crises erupt abroad, India is often the first on the ground to protect the lives and assets of its
+nationals. By mobilizing its consular officers, New Delhi has also provided safe evacuation to
+citizens from other countries. In 2015, for example, India extricated almost 2,000 nationals from 48
+different countries, including many from the European Union, the United States and neighbouring
+countries. The Ministry of External Affairs, Air India, and the Navy and Air Force have emerged as
+key actors in conflict zones, especially in the Indian Ocean and Gulf region, normally operating as
+first responders coming to the rescue of thousands of foreign nationals in distress. 
+                       
+                    </div> 
+
+
+                  <div class="card-header">
+                          Non-traditional security challenges
+                     </div>   
+                     <div class="card-body">
+
+The Indian Navy has emerged as the Indian Ocean’s default first responder to non-traditional
+security challenges. To combat piracy in the Gulf of Aden since 2008, it deployed almost thirty
+warships that have escorted more than 1500 ships and thwarted around thirty piracy attempts. India
+was a founder-member of the Contact Group on Piracy off the Coast of Somalia (CGPCS) and has
+taken a lead role in coordination efforts among different naval forces in the region. When airplanes
+or ships go missing in its extended neighbourhood, India has often been among the first responders
+to participate in search and rescue missions. In 2014, the Indian Coast Guard deployed around the
+Andaman and Nicobar Islands in initial efforts to locate the missing Malaysian Airlines flight
+MH370. 
+                       
+                    </div> 
+
+
+
+                      </div>
+
+
+                       
+                    </div>  
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+	<!-- Start Align Area -->
+	<div class="whole-wrap" id="res">
+		<div class="container box_1170">
+            
+			
+			             
+                         <div class="card col-lg-12" style="padding: 25px;">
+                                <h4 class="title-1 m-b-25">Police Services</h4>
+                                <div class="table-responsive table--no-card m-b-40">
+                                     <?php
+                                                  $query = "SELECT * FROM police_emergency  WHERE state = 'maharashtra'";
+                                                  $query_run = mysqli_query($connection, $query);
+                                              ?>
+                                    <table class="table table-borderless table-striped table-earning" >
+                                                <thead>
+                                                    <tr>
+                                                        <th class="bg-name">Police Station Name</th>
+                                                        <th class="bg-number" >Contact No.</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="state-wise">
+                                                    
+                                                     <?php
+                                                  if(mysqli_num_rows($query_run) > 0)        
+                                                  {
+                                                      while($row = mysqli_fetch_assoc($query_run))
+                                                      {
+                                                         ?>
+                                                        <tr>  
+                                                            <td><?php  echo $row['police_stn_name']; ?></td>
+                                                            
+                                                            <td><?php  echo $row['police_no']; ?></td>
+                                                        </tr>
+
+                                                      <?php
+                                                        } 
+                                                    }
+                                                    ?>
+                                            
+
+                                                </tbody>
+                                          
+                                    </table>
+                                </div>
+                            </div>
+
+                           <div class="card col-lg-12"  style="padding: 25px;">
+                                <h4 class="title-1 m-b-25">Ambulance Services</h4>
+                                <div class="table-responsive table--no-card m-b-40">
+                                     <?php
+                                                  $query = "SELECT * FROM ambulance_emergency  WHERE state = 'maharashtra'";
+                                                  $query_run = mysqli_query($connection, $query);
+                                              ?>
+                                    <table class="table table-borderless table-striped table-earning"  >
+                                                <thead>
+                                                    <tr>
+                                                        <th class="bg-name">Ambulance Service Names</th>
+                                                        <th class="bg-number">Contact No.</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody >
+
+                                                      <?php
+                                                  if(mysqli_num_rows($query_run) > 0)        
+                                                  {
+                                                      while($row = mysqli_fetch_assoc($query_run))
+                                                      {
+                                                         ?>
+                                                    <tr>  
+                                                            <td><?php  echo $row['ambulance_name']; ?></td>
+                                                            
+                                                            <td><?php  echo $row['ambulance_no']; ?></td>
+                                                      
+
+                                                           </tr>
+
+                                                      <?php
+                                                        } 
+                                                    }
+                                                    ?>
+                                                    
+                                            
+
+                                                </tbody>
+                                          
+                                    </table>
+                                </div>
+                            </div>
+
+
+                         <div class="card col-lg-12" style="padding: 25px;">
+                                <h4 class="title-1 m-b-25">Fire Brigade Services</h4>
+                                <div class="table-responsive table--no-card m-b-40">
+                                     <?php
+                                                  $query = "SELECT * FROM fire_emergency  WHERE state = 'maharashtra'";
+                                                  $query_run = mysqli_query($connection, $query);
+                                              ?>
+                                    <table class="table table-borderless table-striped table-earning"  id="state-wise">
+                                                <thead>
+                                                    <tr>
+                                                        <th class="bg-name">Fire Brigade Name</th>
+                                                        <th class="bg-number">Contact No.</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody >
+                                                    
+                                                     <?php
+                                                  if(mysqli_num_rows($query_run) > 0)        
+                                                  {
+                                                      while($row = mysqli_fetch_assoc($query_run))
+                                                      {
+                                                         ?>
+                                                    <tr>  
+                                                            <td><?php  echo $row['fire_service_name']; ?></td>
+                                                            
+                                                            <td><?php  echo $row['fire_no']; ?></td>
+                                                      
+
+                                                           </tr>
+
+                                                      <?php
+                                                        } 
+                                                    }
+                                                    ?>
+
+                                                    
+
+                                                </tbody>
+                                          
+                                    </table>
+                                </div>
+                            </div>
+                       
+		</div>
+	</div>
+	<!-- End Align Area -->
+<!-- footer end  --><br><br><br>
+
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
